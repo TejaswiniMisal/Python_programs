a52d59732d04346b8ef5782cfc5cca41286619f0
Tejaswini Misal <163717058+TejaswiniMisal@users.noreply.github.com>
2025-03-01 22:02:22 +0530

    Add files via upload

---
+++ dictt.py
@@ -0,0 +1,5 @@
+dict1 ={ "Name": "Tejaswni","Class": "MCA", "Age": 21, "Gender": "Female", "Address": "Ghorpadi"}
+dict2 ={"College": "ADYPU", "Hobbies": "Singing", "Birthdate": 22, "status": "single", "Phone_no": 123654 }
+
+dict1.update(dict2)
+print(dict1)
